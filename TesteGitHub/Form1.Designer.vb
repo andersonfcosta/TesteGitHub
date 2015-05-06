a2dcd5ad7d94546dd1019142da6d89f0f32c5e84
andersonfc <anderson.lari.davi@gmail.com>
2015-05-06 20:33:15 -0300

    Conexão com MYSQL e carregamento de combo
    Criada a Função PreencheCombo
    Conectado e Retornado os dados do servidor

--- TesteGitHub/Form1.Designer.vb
+++ TesteGitHub/Form1.Designer.vb
@@ -24,14 +24,15 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.cbbEmail = New System.Windows.Forms.ComboBox()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(77, 55)
+        Me.Button1.Location = New System.Drawing.Point(336, 12)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(293, 64)
+        Me.Button1.Size = New System.Drawing.Size(109, 21)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
@@ -44,11 +45,20 @@ Partial Class Form1
         Me.DataGridView1.Size = New System.Drawing.Size(431, 268)
         Me.DataGridView1.TabIndex = 1
         '
+        'cbbEmail
+        '
+        Me.cbbEmail.FormattingEnabled = True
+        Me.cbbEmail.Location = New System.Drawing.Point(12, 12)
+        Me.cbbEmail.Name = "cbbEmail"
+        Me.cbbEmail.Size = New System.Drawing.Size(318, 21)
+        Me.cbbEmail.TabIndex = 2
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(468, 413)
+        Me.Controls.Add(Me.cbbEmail)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
@@ -59,5 +69,6 @@ Partial Class Form1
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents cbbEmail As System.Windows.Forms.ComboBox
 
 End Class
--- TesteGitHub/Form1.vb
+++ TesteGitHub/Form1.vb
@@ -1,38 +1,57 @@
 ﻿Imports MySql.Data.MySqlClient
 
+
 Public Class Form1
+    Public strConexo As String = "server=landshosting.com.br;user id=lands_conexao;password=conexao2015*;database=lands_eventos"
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim conn As New MySqlConnection
-        Dim MySqlCommand As New MySqlCommand
-        Dim MySQLAdapter As MySqlDataAdapter
-        Dim MySQLDataSet As DataSet
-        Dim strSQL As String
+        Dim objConn As New MySqlConnection(strConexo)
+        Dim objMySQLAdapter As MySqlDataAdapter
+        Dim objMySQLDataSet As New DataSet
 
-        'Pegar os dados no SKYPE
-        'segunda linha de comentário
-
-        conn.ConnectionString = "server=localhost;user id=root;password=mac;database=northwind"
-
-        strSQL = "SELECT * FROM northwind.employees"
+        'Dados servidor Lands
+        '---------------------------
+        'Servidor = landshosting.com.br
+        'usuario = lands_conexao
+        'senha = conexao2015*
+        'database = lands_eventos
+        'http://painel.landsdigital.com.br/adminer/adminer/adminer
+        '---------------------------
 
         Try
-            conn.Open()
+            objConn.Open()
             Try
-                MySqlCommand.Connection = conn
-                MySqlCommand.CommandText = strSQL
-                MySQLAdapter.SelectCommand = MySqlCommand
-                MySQLAdapter.Fill(MySQLDataSet)
-                DataGridView1.DataSource = MySQLDataSet
+                objMySQLAdapter = New MySqlDataAdapter("SELECT usuarios.email_txf, pago_sel,formulario_txf ,evento_txf " _
+                                                     & "FROM usuarios INNER JOIN inscricoes " _
+                                                     & "ON usuarios.email_txf = usuarios.email_txf", objConn)
+                objMySQLAdapter.Fill(objMySQLDataSet, "usuarios")
+                DataGridView1.DataSource = objMySQLDataSet.Tables(0)
             Catch myerro As MySqlException
                 MsgBox("Erro de leitura no banco de dados : " & myerro.Message)
             End Try
-            MessageBox.Show("Conexão aberta com sucesso")
-            conn.Close()
+            objConn.Close()
         Catch myerro As MySqlException
             MessageBox.Show("Erro ao conectar com o Banco de dados : " & myerro.Message)
         Finally
-            conn.Dispose()
+            objConn.Dispose()
         End Try
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cbbEmail = PreencheCombo(cbbEmail, "SELECT email_txf FROM usuarios", "usuarios", "email_txf")
+    End Sub
+
+    Public Function PreencheCombo(ByRef ObjetoComboBox As ComboBox, SQL As String, Tabela As String, Campo As String) As ComboBox
+        Dim objConn As New MySqlConnection(strConexo)
+        Dim objMySQLAdapter As MySqlDataAdapter
+        Dim objMySQLDataSet As New DataSet
+
+        objConn.Open()
+        objMySQLAdapter = New MySqlDataAdapter(SQL, objConn)
+        objMySQLAdapter.Fill(objMySQLDataSet, Tabela)
+        ObjetoComboBox.DataSource = objMySQLDataSet.Tables(0)
+        ObjetoComboBox.DisplayMember = Campo
+
+        Return ObjetoComboBox
+    End Function
 End Class
