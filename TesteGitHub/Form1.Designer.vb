e9e7295ba1e2635d3567b65457b392ca5052d302
andersonfc <anderson.lari.davi@gmail.com>
2015-05-06 23:27:03 -0300

    Criado pesquisa Email e CPF
    Cirado e vinculado as pesquisas

--- TesteGitHub/Form1.Designer.vb
+++ TesteGitHub/Form1.Designer.vb
@@ -22,53 +22,124 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.cbbEmail = New System.Windows.Forms.ComboBox()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.gboEtiqueta = New System.Windows.Forms.GroupBox()
+        Me.lblNome = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.mskCPF = New System.Windows.Forms.MaskedTextBox()
+        Me.btnFiltraDados = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gboEtiqueta.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(336, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(109, 21)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(14, 132)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(431, 268)
-        Me.DataGridView1.TabIndex = 1
         '
         'cbbEmail
         '
+        Me.cbbEmail.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.cbbEmail.FormattingEnabled = True
-        Me.cbbEmail.Location = New System.Drawing.Point(12, 12)
+        Me.cbbEmail.Location = New System.Drawing.Point(144, 11)
         Me.cbbEmail.Name = "cbbEmail"
-        Me.cbbEmail.Size = New System.Drawing.Size(318, 21)
+        Me.cbbEmail.Size = New System.Drawing.Size(212, 21)
         Me.cbbEmail.TabIndex = 2
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.TesteGitHub.My.Resources.Resources.Conexao
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(84, 75)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'gboEtiqueta
+        '
+        Me.gboEtiqueta.BackColor = System.Drawing.Color.White
+        Me.gboEtiqueta.Controls.Add(Me.lblNome)
+        Me.gboEtiqueta.Location = New System.Drawing.Point(12, 93)
+        Me.gboEtiqueta.Name = "gboEtiqueta"
+        Me.gboEtiqueta.Size = New System.Drawing.Size(344, 100)
+        Me.gboEtiqueta.TabIndex = 6
+        Me.gboEtiqueta.TabStop = False
+        '
+        'lblNome
+        '
+        Me.lblNome.AutoSize = True
+        Me.lblNome.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNome.Location = New System.Drawing.Point(6, 14)
+        Me.lblNome.Name = "lblNome"
+        Me.lblNome.Size = New System.Drawing.Size(277, 25)
+        Me.lblNome.TabIndex = 0
+        Me.lblNome.Text = "Anderson Ferreira da Costa"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(102, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "E-mail"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(102, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "CPF"
+        '
+        'mskCPF
+        '
+        Me.mskCPF.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.mskCPF.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mskCPF.Location = New System.Drawing.Point(197, 37)
+        Me.mskCPF.Mask = "999,999,999-99"
+        Me.mskCPF.Name = "mskCPF"
+        Me.mskCPF.Size = New System.Drawing.Size(159, 22)
+        Me.mskCPF.TabIndex = 10
+        Me.mskCPF.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnFiltraDados
+        '
+        Me.btnFiltraDados.Location = New System.Drawing.Point(105, 65)
+        Me.btnFiltraDados.Name = "btnFiltraDados"
+        Me.btnFiltraDados.Size = New System.Drawing.Size(251, 22)
+        Me.btnFiltraDados.TabIndex = 11
+        Me.btnFiltraDados.Text = "Filtra Dados"
+        Me.btnFiltraDados.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(468, 413)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(365, 210)
+        Me.Controls.Add(Me.btnFiltraDados)
+        Me.Controls.Add(Me.mskCPF)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.gboEtiqueta)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.cbbEmail)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gboEtiqueta.ResumeLayout(False)
+        Me.gboEtiqueta.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
     Friend WithEvents cbbEmail As System.Windows.Forms.ComboBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents gboEtiqueta As System.Windows.Forms.GroupBox
+    Friend WithEvents lblNome As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents mskCPF As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents btnFiltraDados As System.Windows.Forms.Button
 
 End Class
--- TesteGitHub/Form1.vb
+++ TesteGitHub/Form1.vb
@@ -4,28 +4,43 @@
 Public Class Form1
     Public strConexo As String = "server=landshosting.com.br;user id=lands_conexao;password=conexao2015*;database=lands_eventos"
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'cbbEmail = PreencheCombo(cbbEmail, "SELECT email_txf FROM usuarios", "usuarios", "email_txf")
+    End Sub
+
+    Private Sub btnFiltraDados_Click(sender As Object, e As EventArgs) Handles btnFiltraDados.Click
+        If mskCPF.TextLength = 14 And mskCPF.Text <> "   .   .   -" Then
+            cbbEmail = PreencheCombo(cbbEmail, "SELECT email_txf FROM usuarios WHERE Cpf_txf = '" & mskCPF.Text & "' ", "usuarios", "email_txf")
+        Else
+            'Filtra os dados na Combo
+            cbbEmail = PreencheCombo(cbbEmail, "SELECT email_txf FROM usuarios WHERE email_txf LIKE '%" & cbbEmail.Text & "%'", "usuarios", "email_txf")
+        End If
+    End Sub
+
+    Private Sub mskCPF_TextChanged(sender As Object, e As EventArgs) Handles mskCPF.TextChanged
+        If mskCPF.Text <> "   .   .   -" Then
+            cbbEmail.Enabled = False
+            cbbEmail.DataSource = Nothing
+        Else
+            cbbEmail.Enabled = True
+        End If
+    End Sub
+
+    Private Sub cbbEmail_LostFocus(sender As Object, e As EventArgs) Handles cbbEmail.LostFocus
         Dim objConn As New MySqlConnection(strConexo)
         Dim objMySQLAdapter As MySqlDataAdapter
-        Dim objMySQLDataSet As New DataSet
-
-        'Dados servidor Lands
-        '---------------------------
-        'Servidor = landshosting.com.br
-        'usuario = lands_conexao
-        'senha = conexao2015*
-        'database = lands_eventos
-        'http://painel.landsdigital.com.br/adminer/adminer/adminer
-        '---------------------------
+        Dim dtsMySQL As New DataSet
 
         Try
             objConn.Open()
             Try
-                objMySQLAdapter = New MySqlDataAdapter("SELECT usuarios.email_txf, pago_sel,formulario_txf ,evento_txf " _
+                objMySQLAdapter = New MySqlDataAdapter("SELECT usuarios.email_txf, pago_sel,formulario_txf, evento_txf " _
                                                      & "FROM usuarios INNER JOIN inscricoes " _
-                                                     & "ON usuarios.email_txf = usuarios.email_txf", objConn)
-                objMySQLAdapter.Fill(objMySQLDataSet, "usuarios")
-                DataGridView1.DataSource = objMySQLDataSet.Tables(0)
+                                                     & "ON usuarios.email_txf = usuarios.email_txf " _
+                                                     & "WHERE usuarios.email_txf LIKE '%" & cbbEmail.Text.ToString & "%'", objConn)
+                objMySQLAdapter.Fill(dtsMySQL, "usuarios")
+                cbbEmail.DataSource = dtsMySQL.Tables(0)
+                'mskCPF.Text = dtsMySQL.Tables(0).Rows.Item("cpf_txf").ToString
             Catch myerro As MySqlException
                 MsgBox("Erro de leitura no banco de dados : " & myerro.Message)
             End Try
@@ -37,8 +52,14 @@ Public Class Form1
         End Try
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        cbbEmail = PreencheCombo(cbbEmail, "SELECT email_txf FROM usuarios", "usuarios", "email_txf")
+
+    Private Sub cbbEmail_KeyDown(sender As Object, e As KeyEventArgs) Handles cbbEmail.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            cbbEmail = PreencheCombo(cbbEmail, "SELECT email_txf FROM usuarios WHERE email_txf LIKE '%" & cbbEmail.Text & "%'", "usuarios", "email_txf")
+        End If
+        If e.KeyCode = Keys.Delete Then
+            cbbEmail.DataSource = Nothing
+        End If
     End Sub
 
     Public Function PreencheCombo(ByRef ObjetoComboBox As ComboBox, SQL As String, Tabela As String, Campo As String) As ComboBox
@@ -54,4 +75,38 @@ Public Class Form1
 
         Return ObjetoComboBox
     End Function
+
+    'Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    '    Dim objConn As New MySqlConnection(strConexo)
+    '    Dim objMySQLAdapter As MySqlDataAdapter
+    '    Dim objMySQLDataSet As New DataSet
+
+    '    'Dados servidor Lands
+    '    '---------------------------
+    '    'Servidor = landshosting.com.br
+    '    'usuario = lands_conexao
+    '    'senha = conexao2015*
+    '    'database = lands_eventos
+    '    'http://painel.landsdigital.com.br/adminer/adminer/adminer
+    '    '---------------------------
+
+    '    Try
+    '        objConn.Open()
+    '        Try
+    '            objMySQLAdapter = New MySqlDataAdapter("SELECT usuarios.email_txf, pago_sel,formulario_txf ,evento_txf " _
+    '                                                 & "FROM usuarios INNER JOIN inscricoes " _
+    '                                                 & "ON usuarios.email_txf = usuarios.email_txf", objConn)
+    '            objMySQLAdapter.Fill(objMySQLDataSet, "usuarios")
+    '            DataGridView1.DataSource = objMySQLDataSet.Tables(0)
+    '        Catch myerro As MySqlException
+    '            MsgBox("Erro de leitura no banco de dados : " & myerro.Message)
+    '        End Try
+    '        objConn.Close()
+    '    Catch myerro As MySqlException
+    '        MessageBox.Show("Erro ao conectar com o Banco de dados : " & myerro.Message)
+    '    Finally
+    '        objConn.Dispose()
+    '    End Try
+    'End Sub
+
 End Class
